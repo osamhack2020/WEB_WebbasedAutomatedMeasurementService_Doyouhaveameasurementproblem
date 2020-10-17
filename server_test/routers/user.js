6d2cbbfb698ab5764eeba0efae384219496cad76
fjvbn2003 <fjvbn2003@gmail.com>
2020-10-17 15:58:21 +0000

    register api added

--- server_test/routers/user.js
+++ server_test/routers/user.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../dbconnection');
-
-router.get('/test', (req, res) => {
+//전체 회원 출력
+router.get('/getUsers', (req, res) => {
   db.query('select * from users', (err, rows) => {
     if (!err) {
       //console.log(rows);
@@ -14,4 +14,28 @@ router.get('/test', (req, res) => {
     }
   });
 });
+
+//회원가입 (post 방식)
+router.post('/register', (req, res) => {
+  const idusers = req.body.idusers;
+  const password = req.body.password;
+  const name = req.body.name;
+  const region = req.body.region;
+  const isAdmin = req.body.isAdmin;
+  const rank = req.body.rank;
+  console.log([idusers, password, name, region, isAdmin, rank]);
+  db.query(
+    'INSERT INTO users (idusers, password,name,region,isAdmin,rank)VALUES (?,?,?,?,?,?)',
+    [idusers, password, name, region, isAdmin, rank],
+    (err, rows) => {
+      if (err) {
+        //console.log(rows);
+        res.json(rows);
+      } else {
+        res.send(rows);
+      }
+    },
+  );
+});
+
 module.exports = router;
