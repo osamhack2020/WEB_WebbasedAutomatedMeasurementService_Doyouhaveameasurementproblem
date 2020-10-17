323cf79206e41149a47c914b8f2d2b299545777e
fjvbn2003 <fjvbn2003@gmail.com>
2020-10-17 15:02:06 +0000

    dbconnection host, port updated

--- server_test/dbconnection.js
+++ server_test/dbconnection.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 const connection = mysql.createPool({
-  host: '54.180.195.102',
-  port: 55308,
+  host: '13.124.194.183',
+  port: 54443,
   user: 'test',
   password: '1234',
   database: 'osam',
