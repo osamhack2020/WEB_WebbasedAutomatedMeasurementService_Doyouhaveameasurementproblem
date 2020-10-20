9ea10e0e0699718846906e05eaa09bccd9d13ea9
NevErdiEkilLeR <ljygenius@gmail.com>
2020-10-21 03:05:46 +0900

    merge

--- server_test/routers/procedure.js
+++ server_test/routers/procedure.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const db = require('../dbconnection');
 
 //전체 측정절차 출력
-<<<<<<< HEAD
 router.get('/procedure/getProcedures', (req,res) => {
    db.query('select * from procedure', (err, rows) => {
         if(!err) {
@@ -13,17 +12,5 @@ router.get('/procedure/getProcedures', (req,res) => {
             res.send(err);
         }
     });
-=======
-router.get('/getProcedures', (req, res) => {
-  db.query('select * from procedure', (err, rows) => {
-    if (!err) {
-      res.json(rows);
-    } else {
-      console.log('Query error : $(err)');
-      res.send(err);
-    }
-  });
->>>>>>> 93da773bcd41d3a2fad586ab7c21a482252a5177
-});
 
 module.exports = router;
