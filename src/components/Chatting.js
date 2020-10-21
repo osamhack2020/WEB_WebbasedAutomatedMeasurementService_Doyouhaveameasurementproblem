ecb503e9595a155486e106543f824f4e883558f1
fjvbn2003 <fjvbn2003@gmail.com>
2020-10-21 19:04:03 +0000

    Chatting.js added

---
+++ src/components/Chatting.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Chatting extends Component {
+  render() {
+    return (
+      <div className="d-flex flex-column align-items-center justify-content-center">
+        채팅창 화면입니다.
+      </div>
+    );
+  }
+}
+
+export default Chatting;
--- src/components/User.js
+++ src/components/User.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import '../css/User.css';
 import cookie from 'react-cookies';
 import Manual from '../components/Manual';
+import Chatting from '../components/Chatting';
 class User extends Component {
   constructor(props) {
     super(props);
@@ -28,6 +29,37 @@ class User extends Component {
           연결 방법 자세히 보기
         </button>
         <Manual id="manual" />
+        <div class="d-block w-50">
+          <div className="progress">
+            <div
+              className="progress-bar"
+              role="progressbar"
+              aria-valuenow="25"
+              aria-valuemin="0"
+              aria-valuemax="100"
+              style={{ width: '25%' }}
+            >
+              25%
+            </div>
+          </div>
+        </div>
+        <div className="d-flex flex-row align-items-stretch ">
+          <Chatting />
+          <ul class="list-group">
+            <li class="list-group-item list-group-item-success">
+              0. 장비 연결 테스트
+            </li>
+            <li class="list-group-item list-group-item-success">
+              1. 전압 DC 5V 테스트
+            </li>
+            <li class="list-group-item list-group-item-secondary">
+              2. 전류 20A 테스트
+            </li>
+            <li class="list-group-item ">3. 주파수 10KHz 테스트</li>
+            <li class="list-group-item ">4. 주기 0.005 sec 테스트</li>
+            <li class="list-group-item ">5. 저항 10Ω 테스트</li>
+          </ul>
+        </div>
       </div>
     );
   }
