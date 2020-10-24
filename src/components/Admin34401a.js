89632dcba205aa5967fd084c525d04dc2454b892
NevErdiEkilLeR <ljygenius@gmail.com>
2020-10-25 05:25:43 +0900

    remind a past bootstrap

--- src/components/Admin34401a.js
+++ src/components/Admin34401a.js
@@ -218,7 +218,7 @@ class Admin34401a extends Component {
     }
     //console.log(procedurelist_items);
     return (
-      <div className="d-flex flex float-md-left align-items-center">
+      <div className="d-flex flex-column justify-content-center align-items-center">
         <div className="flex-row m-2">
           <div className="d-flex-inline p-1 bg-secondary">
             {'아이디 :' +
