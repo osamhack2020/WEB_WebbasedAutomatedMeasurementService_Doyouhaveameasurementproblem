652f938de5cb927069364ffbbcc1042dacd0186e
Nev3rd1Ekil1eR <ljygenius@gmail.com>
2020-10-26 00:38:12 +0900

    Update Manual.js
    Update Slide -Image function

--- src/components/Manual.js
+++ src/components/Manual.js
@@ -23,64 +23,75 @@ class Manual extends Component {
                 data-dismiss="modal"
                 aria-label="Close"
               >
-                <span aria-hidden="true">&times;</span>
+              <span aria-hidden="true">&times;</span>
               </button>
             </div>
+            
+            
             <div className="modal-body">
-              <h5 className="modal-title" id="exampleModalLabel">
-                1. 직류전압 측정 연결 단자
-              </h5>
-              <img
-                src={process.env.PUBLIC_URL + '/DCV_measure_.jpg'}
-                class="img-fluid"
-                alt="image1"
-              />
-              <h5 className="modal-title" id="exampleModalLabel">
-                2. 교류전압 측정 연결 단자
-              </h5>
-              <img
-                src={process.env.PUBLIC_URL + '/ACV_measure_.jpg'}
-                class="img-fluid"
-                alt="image2"
-              />
-              <h5 className="modal-title" id="exampleModalLabel">
-                3. 2W 저항 측정 연결 단자
-              </h5>
-              <img
-                src={process.env.PUBLIC_URL + '/RES_measure_.jpg'}
-                class="img-fluid"
-                alt="image3"
-              />
-              <h5 className="modal-title" id="exampleModalLabel">
-                4. 주파수 측정 연결 단자
-              </h5>
-              <img
-                src={process.env.PUBLIC_URL + '/FREQ_measure_.jpg'}
-                class="img-fluid"
-                alt="image4"
-              />
-              <h5 className="modal-title" id="exampleModalLabel">
-                5. 주기 측정 연결 단자
-              </h5>
-              <img
-                src={process.env.PUBLIC_URL + '/PER_measure_.jpg'}
-                class="img-fluid"
-                alt="image5"
-              />
+              <div id="carousel" class="carousel slide" data-ride="carousel">
+                <div className="carousel-inner">
+                  <div className="carousel-item active">
+                    <h5 className="modal-title" id="exampleModalLabel">
+                      1. 직류전압 측정 연결 단자 </h5>
+                    <img
+                      src={process.env.PUBLIC_URL + '/DCV_measure_.jpg'}
+                      class="img-fluid"
+                      alt="image1"
+                    />
+                  </div>
+                  <div className="carousel-item">
+                    <h5 className="modal-title" id="exampleModalLabel">
+                      2. 교류전압 측정 연결 단자 </h5>
+                    <img
+                      src={process.env.PUBLIC_URL + '/ACV_measure_.jpg'}
+                      class="img-fluid"
+                      alt="image2"
+                    />
+                  </div>
+                  <div className="carousel-item">
+                    <h5 className="modal-title" id="exampleModalLabel">
+                      3. 2W 저항 측정 연결 단자 </h5>
+                    <img
+                      src={process.env.PUBLIC_URL + '/RES_measure_.jpg'}
+                      class="img-fluid"
+                      alt="image3"
+                    />
+                  </div>
+                  <div className="carousel-item">
+                    <h5 className="modal-title" id="exampleModalLabel">
+                      4. 주파수 측정 연결 단자 </h5>
+                    <img
+                      src={process.env.PUBLIC_URL + '/FREQ_measure_.jpg'}
+                      class="img-fluid"
+                      alt="image4"
+                    />
+                  </div>
+                  <div className="carousel-item">
+                    <h5 className="modal-title" id="exampleModalLabel">
+                      5. 주기 측정 연결 단자 </h5>
+                    <img
+                      src={process.env.PUBLIC_URL + '/PER_measure_.jpg'}
+                      class="img-fluid"
+                      alt="image5"
+                    />
+                  </div>
+                </div>
+                <a className="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
+                  <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span className="sr-only">이전</span>
+                </a>
+                <a className="carousel-control-next" href="#carousel" role="button" data-slide="next">
+                  <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span className="sr-only">다음</span>
+                </a>
+              </div>
             </div>
-            <div className="modal-footer">
-              <button
-                type="button"
-                onClick={this.handleClick}
-                className="btn btn-primary"
-                data-dismiss="modal"
-              >
-                닫기
-              </button>
-            </div>
+             
           </div>
         </div>
       </div>
+    
     );
   }
 }
