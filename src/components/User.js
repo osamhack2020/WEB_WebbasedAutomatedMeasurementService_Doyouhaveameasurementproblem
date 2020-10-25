af4cf9d4796106ee6ea01f8bafd0a5e091559490
Neverdiekiller <ljygenius@gmail.com>
2020-10-26 01:54:05 +0900

    Update User.js files

--- src/components/User.js
+++ src/components/User.js
@@ -89,69 +89,76 @@ class User extends Component {
     return (
       <div className="d-flex flex-column align-items-center justify-content-center bg-light">
         User.js 장비를 직접 측정하는 사용자의 화면입니다.
-        <div className="topPannel flex-row  mt-4 bg-secondary">
-          <button className=" btn-sm p-2 mr-4" onClick={this.props.onLogout}>
-            로그아웃
-          </button>
-          <button
-            className="btn-sm p-2"
-            type="button"
-            data-toggle="modal"
-            data-target="#manual"
-          >
-            연결 방법 자세히 보기
-          </button>
-          <div className="d-sm-inline-flex ml-4 text-white-50">관리자 접속여부: admin</div>
-          <BsCircleFill
-            className="d-sm-inline-flex ml-1"
-            style={{ color: 'green' }}
-          />
-        </div>
-        <Manual className="m-4" id="manual" />
-        <div className="mt-4 flex-row">진행률</div>
-          <div className="d-block w-50">
-            <div className="progress">
-              <div
-                className="progress-bar"
-                role="progressbar"
-                aria-valuenow="25"
-                aria-valuemin="0"
-                aria-valuemax="100"
-                style={{ width: '25%' }}
-              > 25%
-              </div>
+            <div className="topPannel flex-row  mt-4 bg-secondary">
+              <button className=" btn-sm p-2 mr-4" onClick={this.props.onLogout}>
+                로그아웃
+              </button>
+              <button
+                className="btn-sm p-2"
+                type="button"
+                data-toggle="modal"
+                data-target="#manual"
+              >
+                연결 방법 자세히 보기
+              </button>
+              <div className="d-sm-inline-flex ml-4 text-white-50">관리자 접속여부: admin</div>
+              <BsCircleFill
+                className="d-sm-inline-flex ml-1"
+                style={{ color: 'green' }}
+              />
             </div>
-          </div>
+        
+        
         <div className="d-flex flex-row">
-          <UserLeftPannel></UserLeftPannel>
-        </div>
-        <div className="d-flex flex-row align-items-stretch mt-4">
-          <ul class="list-group"></ul>
-          <div
-            id="ChattingPannel"
-            className="d-flex flex-column bg-light overflow-auto"
-          >
-            <div className="msg_history">{this.state.messageHistory}</div>
-            <div className="type_msg">
-              <div className="input_msg_write">
-                <input
-                  onChange={this.onInputChanged}
-                  value={this.state.messageInput}
-                  type="text"
-                  className="write_msg"
-                  placeholder="Type a message"
-                />
+          <div className="d-flex flex-column left ">
+              <Manual className="m-4" id="manual" />
+              <div className="mt-4 flex-column">진행률</div>
+                <div className="d-block">
+                  <div className="progress">
+                    <div
+                      className="progress-bar"
+                      role="progressbar"
+                      aria-valuenow="25"
+                      aria-valuemin="0"
+                      aria-valuemax="100"
+                      style={{ width: '25%' }}
+                    > 25%
+                    </div>
+                  </div>
               </div>
-            </div>
-            <button
-              className="d-flex-inline align-items-end"
-              type="button"
-              onClick={this.sendMessage}
-            >
-              Send
-            </button>
+              <div className="d-flex flex-column">
+                  <UserLeftPannel></UserLeftPannel>
+              </div>          
+            </div>   
+
+
+          <div className="d-flex flex-column right align-items-stretch mt-4">
+            <ul class="list-group"></ul>
+              <div
+                  id="ChattingPannel"
+                  className="d-flex flex-column bg-light overflow-auto"
+              >
+                <div className="msg_history">{this.state.messageHistory}</div>
+                  <div className="type_msg">
+                    <div className="input_msg_write">
+                      <input
+                        onChange={this.onInputChanged}
+                        value={this.state.messageInput}
+                        type="text"
+                        className="write_msg"
+                        placeholder="Type a message"
+                      />
+                    </div>
+                  </div>
+                  <button
+                    className="d-flex-inline align-items-end"
+                    type="button"
+                    onClick={this.sendMessage}
+                  > Send
+                  </button>
+                </div>
+              </div>
           </div>
-        </div>
       </div>
     );
   }
