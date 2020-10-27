901453f9485a49875a456675a0699b76fa110567
fjvbn2003 <fjvbn2003@gmail.com>
2020-10-27 14:44:36 +0000

    progress status width changed.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 
